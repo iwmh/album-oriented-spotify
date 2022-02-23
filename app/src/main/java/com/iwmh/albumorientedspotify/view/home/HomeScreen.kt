e0f29b238b9d743f1cf072880fca421d44f25289
iwmh <hiroshiplusone@gmail.com>
2022-02-23 21:43:36 +0900

    Add descriptions to Home.

--- app/src/main/java/com/iwmh/albumorientedspotify/view/home/HomeScreen.kt
+++ app/src/main/java/com/iwmh/albumorientedspotify/view/home/HomeScreen.kt
@@ -1,5 +1,6 @@
 package com.iwmh.albumorientedspotify.view.home
 
+import androidx.compose.foundation.indication
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.padding
@@ -8,6 +9,7 @@ import androidx.compose.runtime.Composable
 import androidx.compose.runtime.collectAsState
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import androidx.hilt.navigation.compose.hiltViewModel
 import com.iwmh.albumorientedspotify.Screen
 import com.iwmh.albumorientedspotify.view.library.LibraryScreenViewModel
@@ -17,7 +19,17 @@ fun HomeScreen(name: String) {
     val viewModel: HomeScreenViewModel = hiltViewModel()
     val uiState = viewModel.uiState.collectAsState()
     Column() {
-        Text(text = " a list of Spotify podcast information .....")
-        Text(text = "Welcome, " + uiState.value.userID + "!")
+        Text(
+            text = "Welcome, " + uiState.value.userID + "!",
+            fontSize = 25.sp,
+        )
+        Spacer(modifier = Modifier.padding(8.dp))
+        Text(
+            text = "This is Album-Oriented Spotify app.",
+            fontSize = 20.sp
+        )
+        Text(text = "・You can browse contents of playlists which owned by someone.")
+        Text(text = "・Inside playlists, you only see albums. Singles are excluded.")
+        Text(text = "・You can add albums' all tracks to playlists you like by tapping one button (Add this buttons in the setting screen).")
     }
 }
